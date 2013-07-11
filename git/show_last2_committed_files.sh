015c4063b0c0bd24928f29a8f72489a2ee330812
Josh <lazydj98@gmail.com>
2013-07-11 16:33:16 -0400

    added 3 git diff commands

--- git/show_last2_committed_files.sh
+++ git/show_last2_committed_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 git diff --stat HEAD~2
+
+# show differences between index and working tree
+# that is, changes you haven't staged to commit
+git diff [filename]
+# show differences between current commit and index
+# that is, what you're about to commit
+git diff --cached [filename]
+# show differences between current commit and working tree
+git diff HEAD [filename]

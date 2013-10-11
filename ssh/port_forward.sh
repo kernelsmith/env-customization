3d0123a647bde8368feded368ccabe3f14be514f
kernelsmith <kernelsmith@metasploit.com>
2013-10-11 15:31:46 -0500

    ssh/portforward now outputs some helpful text

--- ssh/port_forward.sh
+++ ssh/port_forward.sh
@@ -2,10 +2,15 @@
 
 # from the host that needs to get out:
 
-UNUSED_LOCAL_PORT=9999 # You will tell your app, an irc client here, to use localhost and this port to connect
-ULTIMATE_DESTINATION=irc.feenode.net # where you want your app to be able to reach
-ULTIMATE_DESTINATION_PORT=7000 # the port you want your app to be able to reach
-SHELL_SERVER=kernelsmith # ssh config host OR someuser@shellserver where shellserver is an fqdn or ip
+if [ -z $1 ]; then echo "please supply an unused local port" && exit 1;fi
+
+UNUSED_LOCAL_PORT="$1" # You will tell your app, an irc client here, to use localhost and this port to connect
+ULTIMATE_DESTINATION="irc.feenode.net" # where you want your app to be able to reach
+ULTIMATE_DESTINATION_PORT="7000" # the port you want your app to be able to reach
+SHELL_SERVER="kernelsmith" # ssh config host OR someuser@shellserver where shellserver is an fqdn or ip
+LOCALHOST="127.0.0.1" # help's fix some corner case issues.  Sometimes 127.0.0.1, 127.0.1.1, localhost work differently
+# generally it's a bad idea to use 'localhost' because it can have ipv6 confusion
+# http://manas.tungare.name/blog/ssh-port-forwarding-on-mac-os-x/
 
 # in our case, we're simulating an ssh config host:
 # cat ~/.ssh/config
@@ -15,5 +20,7 @@ SHELL_SERVER=kernelsmith # ssh config host OR someuser@shellserver where shellse
 #   PreferredAuthentications publickey # optional, but good for putting in script
 #   IdentityFile ~/.ssh/my_rsa # optional in this case, good if you have > 1, or rename your key file from default (id_rsa)
 
-ssh -L $UNUSED_LOCAL_PORT:$ULTIMATE_DESTINATION:$ULTIMATE_DESTINATION_PORT $SHELL_SERVER
+cmd="ssh -L $LOCALHOST:$UNUSED_LOCAL_PORT:$ULTIMATE_DESTINATION:$ULTIMATE_DESTINATION_PORT $SHELL_SERVER"
+echo "Running:  $cmd"
+$cmd
 # just login (if not using keys) and as long as this ssh session is open, your tunnel is running
